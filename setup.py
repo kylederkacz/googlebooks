d1a969f2ed946a9c969e098f4072418bcfb530b7
Kyle Derkacz <kyle@kylederkacz.com>
2013-10-17 08:40:13 -0700

    Specifying 'requests' dependency

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(name='googlebooks',
       author_email='ph@peter-hoffmann.com',
       url='http://www.python.org/sigs/distutils-sig/',
       py_modules=['googlebooks'],
+      requires=['requests'],
      )
